57ba67a92e27ccece97ecd7016ea1f99e318f4d9
nomadicTree <joshua.cussen@outlook.com>
2016-04-28 20:04:59 -0500

    I commited

--- image-series.rkt
+++ image-series.rkt
@@ -5,7 +5,7 @@
 ; http://www.cs.grinnell.edu/~rebelsky/Courses/CSC151/2016S/eboards/eboard.46.html
 (define int->list
   (lambda (n)
-     (list (quotient n 100) (quotient (remainder n 100) 10) (remainder n 10))))
+    (list (quotient n 100) (quotient (remainder n 100) 10) (remainder n 10))))
 
 (define fancy-int-list
   (lambda (lst)
@@ -14,7 +14,12 @@
 
 
 
-
+(define date-tracker
+  (lambda (n)
+    (let ([year 0]
+          [month 0]
+          [day 0])
+      0)))
 
 
 (define image-series
@@ -28,3 +33,15 @@
                    width
                    height)])
       image)))
+
+(define blend-chooser
+  (lambda (n width height)
+    (let ([row-or-col 0]
+          [max-value (- 256 (* n (quotient 256 9)))]))
+      
+    (when 
+    (image-compute
+     (lambda (col row)
+       
+
+
