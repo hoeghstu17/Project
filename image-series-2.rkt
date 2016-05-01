a9f79238fb88166480bb41083c2201274c6f8f4c
nomadicTree <joshua.cussen@outlook.com>
2016-05-01 13:52:39 -0500

    I commited

--- image-series-2.rkt
+++ image-series-2.rkt
@@ -1,9 +1,7 @@
 #lang racket
 (require gigls/unsafe)
 
-(define world (image-show (image-new 1000 1000)))
-(define test-turtle (turtle-new world))
-(turtle-set-brush! test-turtle "2. Hardness 100")
+
 
 (define sol (list "orangered" "sienna" "orange" "yellow")) ;;; sun colors
 
@@ -122,11 +120,11 @@
            [n-of-polygons 8]
            [n-of-sides 10]
            [polygon-side-length 6])
-      (turtle-down! turtle)
       (turtle-teleport!
        turtle
        (/ (image-width (turtle-world turtle)) 2)
        (/ (image-height (turtle-world turtle)) 2))
+      (turtle-up! turtle)
       (turtle-face! turtle 270)
       (turtle-forward! turtle orbit-radius)
       (turtle-face! turtle 0)
@@ -139,90 +137,109 @@
        (turtle-point turtle)))))
 
 
+(define planet-chooser
+  (lambda (n year)
+    (image-load
+     (cond
+       [(> n 990)
+        "/home/joshua/git/project/planet6.png"]
+       [(equal? year 1)
+        "/home/joshua/git/project/planet1.png"]
+       [(equal? year 2)
+        "/home/joshua/git/project/planet3.png"]
+       [(equal? year 3)
+        "/home/joshua/git/project/planet5.png"]                    
+       [else
+        "/home/joshua/git/project/planet6.png"]))))
 
+(define elements
+  (list (list 1 'ARCANE "purple" "gold")
+        (list 2 'EARTH "forestgreen" "lightblue")
+        (list 3 'WATER "royalblue" "skyblue")
+        (list 4 'FIRE "gray" "orangered")                           
+        (list 5 'AIR "whitesmoke" "skyblue")))
 
-(define image-series
+;;; taken from http://www.cs.grinnell.edu/~rebelsky/Courses/CSC151/2016S/eboards/eboard.46.html
+(define int->list
+  (lambda (int)
+    (list 
+     (quotient int 100)
+     (quotient (remainder int 100) 10)
+     (remainder int 10))))
+
+(define background-blend
   (lambda (n width height)
-    (let* ([n-list (list ;;; taken from http://www.cs.grinnell.edu/~rebelsky/Courses/CSC151/2016S/eboards/eboard.46.html
-                    (quotient n 100)
-                    (quotient (remainder n 100) 10)
-                    (remainder n 10))]
+    (let* ([n-list (int->list n)]
            [n-list-sum (apply + n-list)]
            [n-list-a (car n-list)]
            [n-list-b (cadr n-list)]
            [n-list-c (caddr n-list)]
-           [year (+ 1 (quotient n 365))] ;;; one year is 365 days long, start at year 1
-           [element-season (+ 1 (floor (/ (remainder n 365) 73)))] ;;; 73 days per season, 5 seasons - does not work
-           [plan (image-load
-                  (cond
-                    [(> n 990)
-                     "/home/joshua/git/project/planet6.png"]
-                    [(equal? year 1)
-                     "/home/joshua/git/project/planet1.png"]
-                    [(equal? year 2)
-                     "/home/joshua/git/project/planet3.png"]
-                    [(equal? year 3)
-                     "/home/joshua/git/project/planet5.png"]                    
-                    [else
-                     "/home/joshua/git/project/planet6.png"]))]                                            
-           [elements (list (list 1 'ARCANE "purple" "gold")
-                           (list 2 'EARTH "forestgreen" "lightblue")
-                           (list 3 'WATER "royalblue" "skyblue")
-                           (list 4 'FIRE "gray" "orangered")                           
-                           (list 5 'AIR "whitesmoke" "skyblue"))]
-           [image-blend
-            (let ([max-value
-                   (lambda (val)
-                     (cond ;;; Add more variation here, or change to if
-                       [(even? n-list-sum)
-                        (- 256 (* val (quotient 256 9)))]
-                       [else
-                        (- 256 (* val 28))]))])
-              (image-compute
-               (lambda (x y)
-                 (irgb
-                  (* (if (odd? n-list-a) x y)
-                     (/ (max-value n-list-a)
-                        (if (odd? n-list-a) (- width 1) (- height 1))))
-                  (* (if (odd? n-list-b) x y)
-                     (/ (max-value n-list-b)
-                        (if (odd? n-list-b) (- width 1) (- height 1))))
-                  (* (if (odd? n-list-c) x y)
-                     (/ (max-value n-list-c)
-                        (if (odd? n-list-c) (- width 1) (- height 1))))))
-               width
-               height))]                 
-           [planet-color-changer
-            (lambda (pixel primary secondary)
-              (if (equal? pixel (irgb 255 255 255))
-                  secondary
-                  primary))]
-           [planet-elementifier ;; procedure to check season/element and change colour of planet
-            (lambda (image)
-              (let kernel ([lst elements])
-                (let ([element (car (car lst))])
-                  (if
-                   (equal? element-season element)
-                   (let ([primary (color-name->irgb (caddr (car lst)))]
-                         [secondary (color-name->irgb (cadddr (car lst)))])
-                     (image-transform!
-                      image
-                      (section planet-color-changer ;;; David Neill Asanza helped with sectioning image-transform                          
-                               <>
-                               primary
-                               secondary)))
-                   (kernel (cdr lst))))))]                      
-           [planet (planet-elementifier plan)] ;;; must change variable name
-           [planet-placer ;;; procedure to place planet in image based on code from http://www.cs.grinnell.edu/~rebelsky/Courses/CSC151/2016S/labs/collage-lab.html
-            (lambda ()
-              (let ([planet-width (image-width planet)]
-                    [planet-height (image-width planet)])
-                (image-select-ellipse! planet REPLACE 0 0 planet-width planet-height)
-                (gimp-edit-copy-visible planet)
-                (let ([pasted (car (gimp-edit-paste (image-get-layer image-blend) 1))])
-                  (image-select-ellipse! image-blend REPLACE 0 0 width height)                (image-select-nothing! image-blend)
-                  (gimp-layer-scale pasted (/ width 5) (/ height 5) 1)
-                  (gimp-image-flatten image-blend))))])
-      (planet-placer)
-      (image-show image-blend)
-      )))
+           [max-value
+            (lambda (val)
+              (cond ;;; Add more variation here, or change to if
+                [(even? n-list-sum)
+                 (- 256 (* val (quotient 256 9)))]
+                [else
+                 (- 256 (* val 28))]))])
+      (image-compute
+       (lambda (x y)
+         (irgb
+          (* (if (odd? n-list-a) x y)
+             (/ (max-value n-list-a)
+                (if (odd? n-list-a) (- width 1) (- height 1))))
+          (* (if (odd? n-list-b) x y)
+             (/ (max-value n-list-b)
+                (if (odd? n-list-b) (- width 1) (- height 1))))
+          (* (if (odd? n-list-c) x y)
+             (/ (max-value n-list-c)
+                (if (odd? n-list-c) (- width 1) (- height 1))))))
+       width
+       height))))
+
+(define planet-seasoning
+  (lambda (n planet)
+    (let ([element-season (+ 1 (floor (/ (remainder n 365) 73)))]
+          [planet-color-changer
+           (lambda (pixel primary secondary)
+             (if (equal? pixel (irgb 255 255 255))
+                 secondary
+                 primary))])     
+      (let kernel ([lst elements])
+        (let ([element (car (car lst))])
+          (if (equal? element-season element)
+              (let ([primary (color-name->irgb (caddr (car lst)))]
+                    [secondary (color-name->irgb (cadddr (car lst)))])
+                (image-transform!
+                 planet
+                 (section planet-color-changer ;;; David Neill Asanza helped with sectioning image-transform                          
+                          <>
+                          primary
+                          secondary)))
+              (kernel (cdr lst))))))))
+
+(define planet-placer
+  (lambda (planet image)
+    (let ([width-of-planet (image-width planet)]
+          [height-of-planet(image-width planet)]
+          [width-of-image (image-width image)]
+          [height-of-image (image-height image)])
+      (image-select-ellipse! planet REPLACE 0 0 width-of-planet height-of-planet)
+      (gimp-edit-copy-visible planet)
+      (let ([pasted (car (gimp-edit-paste (image-get-layer image) 1))])
+        (image-select-ellipse! image REPLACE 0 0 width-of-image height-of-image)
+        (image-select-nothing! image)
+        (gimp-layer-scale pasted (/ width-of-image 5) (/ height-of-image 5) 1)
+        (gimp-image-flatten image)))))
+
+
+(define image-series
+  (lambda (n width height)
+    (let* ([year (+ 1 (quotient n 365))]           
+           [planet (planet-chooser n year)]
+           [image (background-blend n width height)]
+           [solaris (turtle-new image)])
+      (image-show image)
+      (planet-seasoning n planet)
+      (planet-placer planet image)
+      (turtle-set-brush! solaris "2. Hardness 100")
+      (satellite-starter solaris n width height))))
